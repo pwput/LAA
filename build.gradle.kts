3d261db4c1feeea81343ff9afe8b41e5953956a0
pwput <45051480+pwput@users.noreply.github.com>
2023-10-01 21:34:33 +0200

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
-allprojects{
+allprojects {
     buildscript {
         dependencies {
             classpath("com.android.tools.build:gradle:8.1.1")
             classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
             classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
         }
+    }
+}
+
+tasks.register("test", Test::class){
+    testLogging {
+        events("started", "skipped", "passed", "failed")
     }
 }
